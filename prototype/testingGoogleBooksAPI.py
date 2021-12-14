ce39e6530f1cbcf7c392bdd5dde3955ccdcca1dc
Jmaxwong <68025573+Jmaxwong@users.noreply.github.com>
2021-12-14 17:53:18 -0500

    added random good title functionality

---
+++ prototype/testingGoogleBooksAPI.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from random import randrange
+
+def get_rand_book(bname):
+    key = "&key=AIzaSyCUbOUuw8As3ge_lxljneppox9WbTHimrU"
+    q = "q=" + bname.replace(" ", "_")
+    url = "https://www.googleapis.com/books/v1/volumes?" + q + key
+    print(url)
+    try:
+        response = requests.request("GET", url)
+        jsonresp = response.json()
+        size = len(jsonresp['items'])
+        print(jsonresp['items'][randrange(size)]['volumeInfo']['title'])
+    except:
+        print("ERROR GET BOOK")
+
+def main():
+    get_book("To Kill A Mockingbird")
+
+if __name__ == "__main__":
+    main()
+    
